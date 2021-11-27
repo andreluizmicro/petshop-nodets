40a4ac0c1563f7b95f07b8f60c94a9a87351ead7
André Luiz da Silva <andreluizmicro@gmail.com>
2021-11-27 10:01:21 -0300

    Criada as telas de visualizaçao de animais

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,18 +1,37 @@
 import { Request, Response } from 'express';
 
 export const index = (req: Request, res: Response) => {
-    res.send('home no Controller');
-    // res.render('pages/page');
+    res.render('pages/index', {
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
 }
 
 export const getDogs = (req: Request, res: Response) => {
-    // res.render('pages/page');
+    res.render('pages/index', {
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    });
 }
 
 export const getCats = (req: Request, res: Response) => {
-    // res.render('pages/page');
+    res.render('pages/index', {
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    });
 }
 
 export const getFishes = (req: Request, res: Response) => {
-    // res.render('pages/page');
+    res.render('pages/index', {
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    });
 }
